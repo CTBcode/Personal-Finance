33b057ba48cf509243d27d595ce914e14a49d57c
ImanuelWilson <ComputerTechBasics@gmail.com>
2022-08-11 21:22:23 +0200

    Update PF_Main.py
    added data for import

--- PF_Main.py
+++ PF_Main.py
@@ -1,5 +1,20 @@
 import csv
-
+#import file
 month = "Imported month Name"
 
 file = f"name_{month}.scv"
+
+transactions = []
+
+with open(file, mode="r") as csv_file:
+    csv_reader = csv.reader(csv_file)
+    for row in csv_reader:
+        date = row[0]
+        name = row[1]
+        amount = float(row[2])
+        transaction = ((date, name, amount))
+        print()
+        transactions.append(transaction)
+
+#sa = FileName.service_account()
+#sh = sa.open("")
